f916c73ca62c9202ea83b35660e21033a1d366d1
randih28 <randihardiansyah280828@gmail.com>
2024-07-29 00:27:21 +0700

    updated

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ logging.basicConfig(filename='app.log', level=logging.DEBUG)
 from routes import *
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
